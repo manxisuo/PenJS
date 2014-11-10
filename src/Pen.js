c152186a9bf184010725b5373c4fa2b49e4039f5
manxisuo <manxisuo@gmail.com>
2014-11-10 21:18:02 +0800

    改变Formatter

--- src/Pen.js
+++ src/Pen.js
@@ -1,248 +1,248 @@
 (function(window) {
-	var Pen = Pen || {};
+    var Pen = Pen || {};
 
-	Pen._scriptList = ['Util.js', 'Loader.js', 'DocUtil.js', 'ClassManager.js', 'Event.js',
-			'Labeling.js', 'Timer.js', 'Stage.js', 'Sprite.js', 'Group.js', 'Sprites.js',
-			'Component.js', 'Shape.js', 'Storage.js', 'Brush.js', 'Tween.js'];
+    Pen._scriptList = ['Util.js', 'Loader.js', 'DocUtil.js', 'ClassManager.js', 'Event.js',
+            'Labeling.js', 'Timer.js', 'Stage.js', 'Sprite.js', 'Group.js', 'Sprites.js',
+            'Component.js', 'Shape.js', 'Storage.js', 'Brush.js', 'Tween.js'];
 
-	Pen.config = {
-		root: '',
-		requires: [],
-		canvas: null,
-	};
+    Pen.config = {
+        root: '',
+        requires: [],
+        canvas: null,
+    };
 
-	Pen.Base = function() {
-	};
+    Pen.Base = function() {
+    };
 
-	Pen.Base.prototype.callParent = function(methodName) {
-		var args = this.callParent.caller.arguments;
-		if (this.__proto__ && this.__proto__[methodName]) {
-			var result = this.__proto__[methodName].apply(this, args);
-			return result;
-		}
-	};
+    Pen.Base.prototype.callParent = function(methodName) {
+        var args = this.callParent.caller.arguments;
+        if (this.__proto__ && this.__proto__[methodName]) {
+            var result = this.__proto__[methodName].apply(this, args);
+            return result;
+        }
+    };
 
-	// TODO
-	Pen.require = function(className) {
-	};
+    // TODO
+    Pen.require = function(className) {
+    };
 
-	Pen.setConfig = function(config) {
-		this.copy(this.config, config);
-	};
+    Pen.setConfig = function(config) {
+        this.copy(this.config, config);
+    };
 
-	/**
-	 * 生成ID。
-	 */
-	Pen.getId = (function() {
-		var id = 0;
+    /**
+     * 生成ID。
+     */
+    Pen.getId = (function() {
+        var id = 0;
 
-		return function() {
-			return 'gen' + (++id);
-		};
-	})();
+        return function() {
+            return 'gen' + (++id);
+        };
+    })();
 
-	Pen.isArray = ('isArray' in Array) ? Array.isArray : function(value) {
-		return toString.call(value) === '[object Array]';
-	};
+    Pen.isArray = ('isArray' in Array) ? Array.isArray : function(value) {
+        return toString.call(value) === '[object Array]';
+    };
 
-	Pen.isSimpleObject = function(v) {
-		return v instanceof Object && v.constructor === Object;
-	};
+    Pen.isSimpleObject = function(v) {
+        return v instanceof Object && v.constructor === Object;
+    };
 
-	/**
-	 * 克隆对象。
-	 * 对于[数组]和[简单对象]，会迭代其[元素]和[属性]。
-	 */
-	Pen.clone = function(source) {
-		if (source != null) {
-			// 对于[数组]，会迭代其[元素]。
-			if (Pen.isSimpleObject(source)) {
-				var obj = {}, p;
-				for (p in source) {
-					obj[p] = Pen.clone(source[p]);
-				}
+    /**
+     * 克隆对象。
+     * 对于[数组]和[简单对象]，会迭代其[元素]和[属性]。
+     */
+    Pen.clone = function(source) {
+        if (source != null) {
+            // 对于[数组]，会迭代其[元素]。
+            if (Pen.isSimpleObject(source)) {
+                var obj = {}, p;
+                for (p in source) {
+                    obj[p] = Pen.clone(source[p]);
+                }
 
-				return obj;
-			}
+                return obj;
+            }
 
-			// 对于[简单对象]，会迭代其[属性]。
-			if (Pen.isArray(source)) {
-				var arr = [], i, len = source.length;
-				for (i = 0; i < len; i++) {
-					arr.push(Pen.clone(source[i]));
-				}
+            // 对于[简单对象]，会迭代其[属性]。
+            if (Pen.isArray(source)) {
+                var arr = [], i, len = source.length;
+                for (i = 0; i < len; i++) {
+                    arr.push(Pen.clone(source[i]));
+                }
 
-				return arr;
-			}
-		}
+                return arr;
+            }
+        }
 
-		return source;
-	};
+        return source;
+    };
 
-	/**
-	 * 将一个对象的属性合并到另一个对象。 注意：不会递归对象的属性，且不会克隆非基本类型的属性。
-	 * 
-	 * @param target 目标对象
-	 * @param source 源对象
-	 * @return 目标对象
-	 */
-	Pen.copy = function(target, source) {
-		if (source && target) {
-			for ( var p in source) {
-				target[p] = Pen.clone(source[p]);
-			}
-		}
+    /**
+     * 将一个对象的属性合并到另一个对象。 注意：不会递归对象的属性，且不会克隆非基本类型的属性。
+     * 
+     * @param target 目标对象
+     * @param source 源对象
+     * @return 目标对象
+     */
+    Pen.copy = function(target, source) {
+        if (source && target) {
+            for ( var p in source) {
+                target[p] = Pen.clone(source[p]);
+            }
+        }
 
-		return target;
-	};
+        return target;
+    };
 
-	/**
-	 * 将一个对象的属性合并到另一个对象。 但是只拷贝目标对象中不存在的属性。 注意：不会递归对象的属性，且不会克隆非基本类型的属性。
-	 * 
-	 * @param target 目标对象
-	 * @param source 源对象
-	 * @return 目标对象
-	 */
-	Pen.copyIf = function(target, source) {
-		if (source && target) {
-			for ( var p in source) {
-				if (target[p] === undefined) {
-					target[p] = Pen.clone(source[p]);
-				}
-			}
-		}
+    /**
+     * 将一个对象的属性合并到另一个对象。 但是只拷贝目标对象中不存在的属性。 注意：不会递归对象的属性，且不会克隆非基本类型的属性。
+     * 
+     * @param target 目标对象
+     * @param source 源对象
+     * @return 目标对象
+     */
+    Pen.copyIf = function(target, source) {
+        if (source && target) {
+            for ( var p in source) {
+                if (target[p] === undefined) {
+                    target[p] = Pen.clone(source[p]);
+                }
+            }
+        }
 
-		return target;
-	};
+        return target;
+    };
 
-	/**
-	 * 加载js脚本。
-	 * 
-	 * @param path 脚本文件
-	 * @param callback 回调函数
-	 */
-	Pen.loadJS = function(path, callback) {
-		var script = document.createElement('script');
-		script.src = path;
-		script.type = 'text/javascript';
-		script.onload = function() {
-			if (callback) {
-				callback();
-			}
-		};
+    /**
+     * 加载js脚本。
+     * 
+     * @param path 脚本文件
+     * @param callback 回调函数
+     */
+    Pen.loadJS = function(path, callback) {
+        var script = document.createElement('script');
+        script.src = path;
+        script.type = 'text/javascript';
+        script.onload = function() {
+            if (callback) {
+                callback();
+            }
+        };
 
-		document.head.appendChild(script);
-	};
+        document.head.appendChild(script);
+    };
 
-	function getFullPath(dir, name) {
-		if (null != dir) {
-			if (dir.charAt(dir.length - 1) != '/') {
-				dir += '/';
-			}
+    function getFullPath(dir, name) {
+        if (null != dir) {
+            if (dir.charAt(dir.length - 1) != '/') {
+                dir += '/';
+            }
 
-			return dir + name;
-		}
+            return dir + name;
+        }
 
-		return name;
-	}
+        return name;
+    }
 
-	/**
-	 * 并行地加载所有脚本。
-	 */
-	Pen._loadAllJsParallelly = function(oncomplete) {
-		var me = this;
-		var root = me.config.root;
-		var list = me._scriptList.concat(me.config.requires), len = list.length, count = 0;
-		var i, script;
+    /**
+     * 并行地加载所有脚本。
+     */
+    Pen._loadAllJsParallelly = function(oncomplete) {
+        var me = this;
+        var root = me.config.root;
+        var list = me._scriptList.concat(me.config.requires), len = list.length, count = 0;
+        var i, script;
 
-		for (i in list) {
-			script = list[i];
+        for (i in list) {
+            script = list[i];
 
-			(function(script) {
-				me.loadJS(getFullPath(root, script), function() {
-					count++;
-					if (count == len) {
-						if (oncomplete) {
-							oncomplete();
-						}
-					}
-				});
-			})(script);
-		}
-	};
+            (function(script) {
+                me.loadJS(getFullPath(root, script), function() {
+                    count++;
+                    if (count == len) {
+                        if (oncomplete) {
+                            oncomplete();
+                        }
+                    }
+                });
+            })(script);
+        }
+    };
 
-	/**
-	 * 串行地加载所有脚本。
-	 */
-	Pen._loadAllJsSerially = function(oncomplete) {
-		var me = this;
-		var root = me.config.root;
-		var list = me._scriptList.concat(me.config.requires), len = list.length, count = 0;
-		var i, script;
+    /**
+     * 串行地加载所有脚本。
+     */
+    Pen._loadAllJsSerially = function(oncomplete) {
+        var me = this;
+        var root = me.config.root;
+        var list = me._scriptList.concat(me.config.requires), len = list.length, count = 0;
+        var i, script;
 
-		var l = [];
-		for (i in list) {
-			script = list[i];
-			(function(script) {
-				l.push(function() {
-					me.loadJS(getFullPath(root, script), function() {
-						l.shift();
-						if (l.length > 0) {
-							l[0]();
-						}
-						else {
-							if (oncomplete) {
-								oncomplete();
-							}
-						}
-					});
-				});
-			})(script);
-		}
+        var l = [];
+        for (i in list) {
+            script = list[i];
+            (function(script) {
+                l.push(function() {
+                    me.loadJS(getFullPath(root, script), function() {
+                        l.shift();
+                        if (l.length > 0) {
+                            l[0]();
+                        }
+                        else {
+                            if (oncomplete) {
+                                oncomplete();
+                            }
+                        }
+                    });
+                });
+            })(script);
+        }
 
-		if (l.length > 0) {
-			l[0]();
-		}
-	};
+        if (l.length > 0) {
+            l[0]();
+        }
+    };
 
-	/**
-	 * 根据实际需要加载所有脚本。
-	 * TODO
-	 */
-	Pen._loadAllJsAsRequired = function(oncomplete) {
-	};
+    /**
+     * 根据实际需要加载所有脚本。
+     * TODO
+     */
+    Pen._loadAllJsAsRequired = function(oncomplete) {
+    };
 
-	/**
-	 * 初始化Pen JS。
-	 */
-	Pen.init = function(callback) {
-		var me = this;
-		me._loadAllJsSerially(function() {
-			var canvas = me.config.canvas;
-			var ctx, brush, stage;
-			ctx = canvas.getContext('2d');
-			brush = new Pen.Brush({
-				canvas: canvas
-			});
-			stage = new Pen.Stage({
-				brush: brush,
-			});
+    /**
+     * 初始化Pen JS。
+     */
+    Pen.init = function(callback) {
+        var me = this;
+        me._loadAllJsSerially(function() {
+            var canvas = me.config.canvas;
+            var ctx, brush, stage;
+            ctx = canvas.getContext('2d');
+            brush = new Pen.Brush({
+                canvas: canvas
+            });
+            stage = new Pen.Stage({
+                brush: brush,
+            });
 
-			Pen.copy(Pen.Global, {
-				canvas: canvas,
-				ctx: ctx,
-				brush: brush,
-				stage: stage
-			});
+            Pen.copy(Pen.Global, {
+                canvas: canvas,
+                ctx: ctx,
+                brush: brush,
+                stage: stage
+            });
 
-			if (callback) {
-				callback();
-			}
-		});
-	};
+            if (callback) {
+                callback();
+            }
+        });
+    };
 
-	Pen.Global = {};
+    Pen.Global = {};
 
-	window.Pen = Pen;
-	window.$P = Pen;
+    window.Pen = Pen;
+    window.$P = Pen;
 })(window);
--- src/Shape.js
+++ src/Shape.js
@@ -1,161 +1,161 @@
 (function(window, Pen) {
-	/**
-	 * 形状。
-	 */
-	Pen.define('Pen.Shape', {
-		extend: Pen.Sprite
-	});
+    /**
+     * 形状。
+     */
+    Pen.define('Pen.Shape', {
+        extend: Pen.Sprite
+    });
 
-	/**
-	 * 圆角矩形
-	 */
-	Pen.define('Pen.RoundRect', {
-		extend: Pen.Shape,
+    /**
+     * 圆角矩形
+     */
+    Pen.define('Pen.RoundRect', {
+        extend: Pen.Shape,
 
-		// 位置和尺寸。
-		x: 50,
-		y: 50,
-		w: 50,
-		h: 30,
+        // 位置和尺寸。
+        x: 50,
+        y: 50,
+        w: 50,
+        h: 30,
 
-		// 四个角的半径。可以是数字(四个角的半径相同)或数组(依次为左上、右上、右下、左下的角的半径)。
-		corners: [5, 5, 5, 5],
+        // 四个角的半径。可以是数字(四个角的半径相同)或数组(依次为左上、右上、右下、左下的角的半径)。
+        corners: [5, 5, 5, 5],
 
-		// 填充色和边框色
-		fillColor: null,
-		borderColor: null,
+        // 填充色和边框色
+        fillColor: null,
+        borderColor: null,
 
-		// 文本
-		text: '',
-		textStyle: {},
-		_textDiv: null,
+        // 文本
+        text: '',
+        textStyle: {},
+        _textDiv: null,
 
-		statics: {
-			FILL_COLOR: '#E7E7E7',
-			BORDER_COLOR: '#D1D1D1',
-		},
+        statics: {
+            FILL_COLOR: '#E7E7E7',
+            BORDER_COLOR: '#D1D1D1',
+        },
 
-		init: function() {
-			var me = this;
+        init: function() {
+            var me = this;
 
-			// 初始化填充色和边框色
-			me.fillColor = me.self.FILL_COLOR;
-			me.borderColor = me.self.BORDER_COLOR;
+            // 初始化填充色和边框色
+            me.fillColor = me.self.FILL_COLOR;
+            me.borderColor = me.self.BORDER_COLOR;
 
-			// 初始化文本
-			if (me.text != null && me.text !== '') {
-				me._initText();
-			}
-		},
-		
-		_initText: function() {
-			var me = this;
-			var div = $('<div />');
-			var css = {
-				'postion': 'absolute',
-				'word-wrap': 'break-word',
-				'text-align': 'center',
-				'vertical-align': 'middle',
-				'overflow': 'hidden',
-				'display': 'none'
-			};
-			Pen.copy(css, me.textStyle);
-			div.css(css);
-			div.addClass('pen-shape-text');
-			div.appendTo($(me.stage.canvas).parent());
-			me._textDiv = div;
-		},
+            // 初始化文本
+            if (me.text != null && me.text !== '') {
+                me._initText();
+            }
+        },
 
-		_drawText: function() {
-			var me = this;
-			
-			if (me._textDiv == null) {
-				me._initText();
-			}
-			
-			var text = me.text, div = me._textDiv, cs = me.corners;
+        _initText: function() {
+            var me = this;
+            var div = $('<div />');
+            var css = {
+                'postion': 'absolute',
+                'word-wrap': 'break-word',
+                'text-align': 'center',
+                'vertical-align': 'middle',
+                'overflow': 'hidden',
+                'display': 'none'
+            };
+            Pen.copy(css, me.textStyle);
+            div.css(css);
+            div.addClass('pen-shape-text');
+            div.appendTo($(me.stage.canvas).parent());
+            me._textDiv = div;
+        },
 
-			if (text && Pen.Util.trim(text)) {
-				var padding = Pen.Util.isNumber(cs) ? cs : Pen.Util.maxArrayItem(cs);
-				div.css({
-					padding: padding + 'px',
-					left: me.x - me.w / 2 + padding + 'px',
-					top: me.y - me.h / 2 + padding + 'px',
-					width: me.w - 2 * padding + 'px',
-					height: me.h = 2 * padding + 'px',
-				// 'line-height': me.h - 2 * padding + 'px'
-				});
-				div.text(me.text);
+        _drawText: function() {
+            var me = this;
 
-				div.show();
-			}
-			else {
-				div.hide();
-			}
-		},
+            if (me._textDiv == null) {
+                me._initText();
+            }
 
-		draw: function(brush, dt) {
-			var me = this;
+            var text = me.text, div = me._textDiv, cs = me.corners;
 
-			brush.roundRect(me.x, me.y, me.w, me.h, me.corners).fill(me.fillColor).stroke(
-					me.borderColor);
+            if (text && Pen.Util.trim(text)) {
+                var padding = Pen.Util.isNumber(cs) ? cs : Pen.Util.maxArrayItem(cs);
+                div.css({
+                    padding: padding + 'px',
+                    left: me.x - me.w / 2 + padding + 'px',
+                    top: me.y - me.h / 2 + padding + 'px',
+                    width: me.w - 2 * padding + 'px',
+                    height: me.h = 2 * padding + 'px',
+                // 'line-height': me.h - 2 * padding + 'px'
+                });
+                div.text(me.text);
 
-			me._drawText();
-		},
+                div.show();
+            }
+            else {
+                div.hide();
+            }
+        },
 
-		checkInside: function(px, py) {
-			var me = this, brush = me.stage.brush;
-			brush.roundRect(me.x, me.y, me.w, me.h, me.corners);
-			
-			return brush.isPointInPath(px, py);
-		},
-	});
+        draw: function(brush, dt) {
+            var me = this;
 
-	/**
-	 * 折线。指可以将两个点链接起来的三条垂直或水平线段组成的折线。
-	 */
-	Pen.define('Pen.Polyline', {
-		extend: Pen.Shape,
-		startX: 100,
-		startY: 100,
-		endX: 200,
-		endY: 200,
-		type: 1,
+            brush.roundRect(me.x, me.y, me.w, me.h, me.corners).fill(me.fillColor).stroke(
+                    me.borderColor);
 
-		// 中间线的坐标。
-		center: 130,
+            me._drawText();
+        },
 
-		color: '#FF00FF',
-		width: 2,
+        checkInside: function(px, py) {
+            var me = this, brush = me.stage.brush;
+            brush.roundRect(me.x, me.y, me.w, me.h, me.corners);
 
-		statics: {
-			// 中间线是水平方向的。
-			X: 1,
+            return brush.isPointInPath(px, py);
+        },
+    });
 
-			// 中间线是 竖直方向的。
-			Y: 2
-		},
-		draw: function(brush, dt) {
-			var me = this, self = me.self;
+    /**
+     * 折线。指可以将两个点链接起来的三条垂直或水平线段组成的折线。
+     */
+    Pen.define('Pen.Polyline', {
+        extend: Pen.Shape,
+        startX: 100,
+        startY: 100,
+        endX: 200,
+        endY: 200,
+        type: 1,
 
-			brush.tmp(function() {
-				brush.setStrokeStyle(me.color);
-				brush.setLineWith(me.width);
-				brush.beginPath();
-				brush.moveTo(me.startX, me.startY);
-				if (me.type == self.X) {
-					brush.lineTo(me.startX, me.center);
-					brush.lineTo(me.endX, me.center);
-					brush.lineTo(me.endX, me.endY);
-				}
-				else {
-					brush.lineTo(me.center, me.startY);
-					brush.lineTo(me.center, me.endY);
-					brush.lineTo(me.endX, me.endY);
-				}
+        // 中间线的坐标。
+        center: 130,
 
-				brush.stroke();
-			});
-		}
-	});
+        color: '#FF00FF',
+        width: 2,
+
+        statics: {
+            // 中间线是水平方向的。
+            X: 1,
+
+            // 中间线是 竖直方向的。
+            Y: 2
+        },
+        draw: function(brush, dt) {
+            var me = this, self = me.self;
+
+            brush.tmp(function() {
+                brush.setStrokeStyle(me.color);
+                brush.setLineWith(me.width);
+                brush.beginPath();
+                brush.moveTo(me.startX, me.startY);
+                if (me.type == self.X) {
+                    brush.lineTo(me.startX, me.center);
+                    brush.lineTo(me.endX, me.center);
+                    brush.lineTo(me.endX, me.endY);
+                }
+                else {
+                    brush.lineTo(me.center, me.startY);
+                    brush.lineTo(me.center, me.endY);
+                    brush.lineTo(me.endX, me.endY);
+                }
+
+                brush.stroke();
+            });
+        }
+    });
 })(window, Pen);
